6db8d870c7aa6f3d8545ad020064be33ce4aeec4
gwenzoeyang <gy101@wellesley.edu>
2025-12-07 23:29:40 -0500

    origin reestablished

--- src/concept_server.ts
+++ src/concept_server.ts
@@ -26,7 +26,7 @@ async function main() {
   app.use(
     "/*",
     cors({
-      origin: "*",
+      origin: "http://localhost:5174",
       allowMethods: ["GET", "POST", "PUT", "DELETE"],
       allowHeaders: ["Content-Type"],
     }),
